cc21d195ad85adbb22d5c08137e1696b9c87736e
Becky Lewis <git@scaryclam.co.uk>
2015-06-22 21:09:01 +0100

    Added launch config

--- src/main/java/uk/co/bluesuntech/autoscaling/AutoscaleClient.java
+++ src/main/java/uk/co/bluesuntech/autoscaling/AutoscaleClient.java
@@ -1,9 +1,13 @@
 package uk.co.bluesuntech.autoscaling;
 
+import java.util.Collection;
+
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.autoscaling.AmazonAutoScaling;
 import com.amazonaws.services.autoscaling.AmazonAutoScalingClient;
+import com.amazonaws.services.autoscaling.model.CreateLaunchConfigurationRequest;
+import com.amazonaws.services.autoscaling.model.InstanceMonitoring;
 
 public class AutoscaleClient {
 	
@@ -13,5 +17,15 @@ public class AutoscaleClient {
 		asClient = new AmazonAutoScalingClient();
 		Region region = Region.getRegion(Regions.EU_WEST_1);
 		asClient.setRegion(region);
+	}
+	
+	public void createLaunchConfiguration(String launchConfigName, String imageId, String instanceType, Collection<String> securityGroups, InstanceMonitoring instanceMonitoring) {
+		CreateLaunchConfigurationRequest lcRequest = new CreateLaunchConfigurationRequest();
+        lcRequest.setLaunchConfigurationName(launchConfigName);
+        lcRequest.setImageId(imageId);
+        lcRequest.setInstanceType(instanceType);
+        lcRequest.setSecurityGroups(securityGroups);
+        lcRequest.setInstanceMonitoring(instanceMonitoring);
+        asClient.createLaunchConfiguration(lcRequest);
 	}
 }
