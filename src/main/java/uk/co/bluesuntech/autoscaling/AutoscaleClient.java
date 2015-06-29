ac0bbe8bc2bff296ade3a9714b7217d89263fc59
Becky Lewis <github@scaryclam.co.uk>
2015-06-29 02:28:16 +0100

    Added more controls

--- src/main/java/uk/co/bluesuntech/autoscaling/AutoscaleClient.java
+++ src/main/java/uk/co/bluesuntech/autoscaling/AutoscaleClient.java
@@ -24,6 +24,12 @@ public class AutoscaleClient {
 	private AmazonAutoScaling asClient;
 	private AmazonCloudWatch cwClient;
 	
+	/* Setting up basic autoscaling is easy.
+	 * 1) Make a launch configuration. This is the EC2 instance information.
+	 * 2) Create your autoscaling group. This sets things like which AZ to run in and which health check type to use.
+	 * 3) Set up the scaling policies. You should also setup the alarms to use with them in this step.
+	 */
+	
 	public AutoscaleClient() {
 		asClient = new AmazonAutoScalingClient();
 		Region region = Region.getRegion(Regions.EU_WEST_1);
@@ -33,12 +39,13 @@ public class AutoscaleClient {
 		cwClient.setRegion(region);
 	}
 	
-	public void createLaunchConfiguration(String launchConfigName, String imageId, String instanceType, Collection<String> securityGroups, InstanceMonitoring instanceMonitoring) {
+	public void createLaunchConfiguration(String launchConfigName, String imageId, String instanceType, Collection<String> securityGroups, String keyName, InstanceMonitoring instanceMonitoring) {
 		CreateLaunchConfigurationRequest request = new CreateLaunchConfigurationRequest();
         request.setLaunchConfigurationName(launchConfigName);
         request.setImageId(imageId);
         request.setInstanceType(instanceType);
         request.setSecurityGroups(securityGroups);
+        request.setKeyName(keyName);
         request.setInstanceMonitoring(instanceMonitoring);
         asClient.createLaunchConfiguration(request);
 	}
@@ -68,7 +75,7 @@ public class AutoscaleClient {
         return result;
 	}
 	
-	public void createScaleUpRequest(String alarmName, Collection<String> actions, Collection<String> metricNames, Collection<Dimension> dimensions, String namespace) {
+	private void createScaleAlarmRequest(String alarmName, Collection<String> actions, Collection<String> metricNames, Collection<Dimension> dimensions, String namespace, ComparisonOperator comparisonOperator, Integer period, Integer evalPeriods, Double threshold) {
 		PutMetricAlarmRequest request = new PutMetricAlarmRequest();
         request.setAlarmName(alarmName);
         
@@ -78,14 +85,22 @@ public class AutoscaleClient {
 
         request.setDimensions(dimensions);
         request.setNamespace(namespace);
-        request.setComparisonOperator(ComparisonOperator.GreaterThanThreshold);
+        request.setComparisonOperator(comparisonOperator);
         request.setStatistic(Statistic.Average);
         request.setUnit(StandardUnit.Percent);
-        request.setThreshold(60d);
-        request.setPeriod(300);
-        request.setEvaluationPeriods(2);
+        request.setThreshold(threshold);
+        request.setPeriod(period);
+        request.setEvaluationPeriods(evalPeriods);
         request.setAlarmActions(actions);
 
         cwClient.putMetricAlarm(request);
+	}
+	
+	public void createScaleUpAlarm(String alarmName, Collection<String> actions, Collection<String> metricNames, Collection<Dimension> dimensions, String nameSpace, Integer period, Integer evalPeriods, Double threshold) {
+        createScaleAlarmRequest(alarmName, actions, metricNames, dimensions, nameSpace, ComparisonOperator.GreaterThanThreshold, period, evalPeriods, threshold);
+	}
+	
+	public void createScaleDownAlarm(String alarmName, Collection<String> actions, Collection<String> metricNames, Collection<Dimension> dimensions, String nameSpace, Integer period, Integer evalPeriods, Double threshold) {
+		createScaleAlarmRequest(alarmName, actions, metricNames, dimensions, nameSpace, ComparisonOperator.LessThanThreshold, period, evalPeriods, threshold);
 	}
 }
--- src/main/java/uk/co/bluesuntech/ec2/EC2Client.java
+++ src/main/java/uk/co/bluesuntech/ec2/EC2Client.java
@@ -3,7 +3,10 @@ package uk.co.bluesuntech.ec2;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
 import org.apache.commons.collections4.keyvalue.MultiKey;
@@ -16,11 +19,14 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.autoscaling.model.Filter;
 import com.amazonaws.services.ec2.AmazonEC2;
 import com.amazonaws.services.ec2.AmazonEC2Client;
+import com.amazonaws.services.ec2.model.AuthorizeSecurityGroupIngressRequest;
 import com.amazonaws.services.ec2.model.CreateSecurityGroupRequest;
 import com.amazonaws.services.ec2.model.CreateSecurityGroupResult;
+import com.amazonaws.services.ec2.model.CreateTagsRequest;
 import com.amazonaws.services.ec2.model.DescribeInstancesRequest;
 import com.amazonaws.services.ec2.model.DescribeInstancesResult;
 import com.amazonaws.services.ec2.model.Instance;
+import com.amazonaws.services.ec2.model.IpPermission;
 import com.amazonaws.services.ec2.model.Reservation;
 import com.amazonaws.services.ec2.model.RunInstancesRequest;
 import com.amazonaws.services.ec2.model.RunInstancesResult;
@@ -28,6 +34,7 @@ import com.amazonaws.services.ec2.model.StartInstancesRequest;
 import com.amazonaws.services.ec2.model.StartInstancesResult;
 import com.amazonaws.services.ec2.model.StopInstancesRequest;
 import com.amazonaws.services.ec2.model.StopInstancesResult;
+import com.amazonaws.services.ec2.model.Tag;
 import com.amazonaws.services.ec2.model.TerminateInstancesRequest;
 import com.amazonaws.services.elasticloadbalancing.model.CreateLoadBalancerRequest;
 import com.amazonaws.services.elasticloadbalancing.model.CreateLoadBalancerResult;
@@ -37,7 +44,7 @@ import com.amazonaws.services.opsworks.model.StopInstanceRequest;
 
 public class EC2Client {
 	private AmazonEC2 ec2Client;
-	private List<Instance> instances;
+	private HashMap<String, Instance> instances;
 	
 	public EC2Client() {
 		System.out.println("Loading ec2 credentials...");
@@ -54,34 +61,72 @@ public class EC2Client {
 	    List<Reservation> reservations = results.getReservations();
 	    for (Reservation reservation : reservations) {
 	    	List<Instance> runningInstances = reservation.getInstances();
+	    	
 	    	if (instances == null) {
-	    	    instances = runningInstances;
-	    	} else {
-	    		instances.addAll(runningInstances);
+	    		instances = new HashMap<String, Instance>();
+	    	}
+	    	
+	    	for (Instance instance : runningInstances) {
+	    		instances.put(instance.getInstanceId(), instance);
 	    	}
 	    }
 	}
 	
 	public void showAllInstances() {
 		System.out.println("These are your instances:");
-		for (Instance instance : instances) {
+		for (Instance instance : instances.values()) {
 			System.out.println(instance);
 		}
 	}
 	
-	public void launchNewInstance(String AMI_ID, String type, Integer number) {
-		RunInstancesRequest request = this.getRunInstanceRequest(AMI_ID, type, number);
-		System.out.println("Launching Instance");
-		RunInstancesResult result = ec2Client.runInstances(request);
-		System.out.println("Launched Instance");
+	public HashMap<String, Instance> getKnownInstances() {
+		return instances;
 	}
 	
-	public void launchNewInstance(String AMI_ID, String type, Integer number, String keyName) {
+	public Collection<Instance> launchNewInstances(String AMI_ID, String type, Integer number) {
+		RunInstancesRequest request = this.getRunInstanceRequest(AMI_ID, type, number);
+		RunInstancesResult result = launchInstances(request);
+		return result.getReservation().getInstances();
+	}
+	
+	public Collection<Instance> launchNewInstances(String AMI_ID, String type, Integer number, String keyName) {
 		RunInstancesRequest request = this.getRunInstanceRequest(AMI_ID, type, number);
 		request.setKeyName(keyName);
+		RunInstancesResult result = launchInstances(request);
+		return result.getReservation().getInstances();
+	}
+	
+	public Collection<Instance> launchNewInstances(String AMI_ID, String type, Integer number, String keyName, Collection<String> sgNames) {
+		RunInstancesRequest request = this.getRunInstanceRequest(AMI_ID, type, number);
+		request.setKeyName(keyName);
+		request.setSecurityGroupIds(sgNames);
+		RunInstancesResult result = launchInstances(request);
+		return result.getReservation().getInstances();
+	}
+	
+	public void addTagsToInstance(Instance instance, Map<String, String> tags) {
+		CreateTagsRequest request = new CreateTagsRequest();
+		Collection<Tag> requestTags = new ArrayList<Tag>();
+		
+		for (Entry<String, String> tagEntry : tags.entrySet()) {
+			Tag tag = new Tag(tagEntry.getKey(), tagEntry.getValue());
+			requestTags.add(tag);
+		}
+		
+		request.withResources(instance.getInstanceId());
+		request.setTags(requestTags);
+		
+		ec2Client.createTags(request);
+	}
+	
+	private RunInstancesResult launchInstances(RunInstancesRequest request) {
 		System.out.println("Launching Instance");
 		RunInstancesResult result = ec2Client.runInstances(request);
 		System.out.println("Launched Instance");
+		for (Instance instance : result.getReservation().getInstances()) {
+			instances.put(instance.getInstanceId(), instance);
+		}
+		return result;
 	}
 	
 	public void stopInstance(String instanceId) {
@@ -101,6 +146,7 @@ public class EC2Client {
 		System.out.println("Terminating Instance");
 		ec2Client.terminateInstances(request);
 		System.out.println("Terminated Instance");
+		instances.remove(instanceId);
 	}
 	
 	public void startInstance(String instanceId) {
@@ -125,5 +171,20 @@ public class EC2Client {
 		System.out.println("Creating security group");
 		CreateSecurityGroupResult result = ec2Client.createSecurityGroup(request);
 		System.out.println("Created security group");
+	}
+	
+	public void addSecurityGroupIpPermission(String sgName, Collection<String> ipRanges, String protocol, Integer startPort, Integer endPort) {
+		IpPermission ipPermission = new IpPermission();
+			    	
+		ipPermission.withIpRanges(ipRanges)
+			        .withIpProtocol(protocol)
+			        .withFromPort(startPort)
+			        .withToPort(endPort);
+		AuthorizeSecurityGroupIngressRequest request = new AuthorizeSecurityGroupIngressRequest();
+			    	
+		request.withGroupName(sgName)
+		       .withIpPermissions(ipPermission);
+	
+		ec2Client.authorizeSecurityGroupIngress(request);
 	}
 }
