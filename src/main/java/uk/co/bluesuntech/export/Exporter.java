14c5af8c209120dbbe924fa0268bb4b6f3113bee
Becky Lewis <github@scaryclam.co.uk>
2015-07-11 12:26:08 +0100

    Turns out AWS already has the json library

--- src/main/java/uk/co/bluesuntech/export/Exporter.java
+++ src/main/java/uk/co/bluesuntech/export/Exporter.java
@@ -1,5 +1,12 @@
 package uk.co.bluesuntech.export;
 
-public class Exporter {
+import com.amazonaws.util.json.JSONException;
+import com.amazonaws.util.json.JSONObject;
 
+public class Exporter {
+	private JSONObject exportExploration() throws JSONException {
+		JSONObject configuration = new JSONObject();
+		configuration.put("foo", "bar");
+		return configuration;
+	}
 }
